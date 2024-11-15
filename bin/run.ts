b780bc96fbda013d32aa0db11416e8f01db0e0bd
Matilde Park <matilde.park@pm.me>
2024-11-15 15:09:17 -0500

    chore: remove starter template on default command

--- bin/run.ts
+++ bin/run.ts
@@ -1,17 +1,11 @@
 import yargs, { CommandModule } from 'yargs'
 import { config } from 'dotenv'
 import { commands } from '../src'
-import { bgBlue, bold, red } from 'picocolors'
+import pc from 'picocolors'
 
 config()
 
-const run = yargs(process.argv.slice(2))
-run.usage(
-  bgBlue(
-    `Welcome to the CLI application powered by ${bold(red('cli-typescript-starter'))}!
-    See more on https://github.com/kucherenko/cli-typescript-starter`,
-  ),
-)
+const run = yargs(process.argv.slice(2)).scriptName('tudor').usage(pc.magenta('Usage: $0 <command> [options]'))
 for (const command of commands) {
   run.command(command as CommandModule)
 }
