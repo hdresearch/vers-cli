17d769601a508d5bd07f3a79fd40734c0d3ad6b8
Matilde Park <matilde.park@pm.me>
2024-11-19 16:22:56 -0500

    chore: no more need for default info command

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,5 +1,4 @@
 import * as auth from "./auth";
 import * as create from "./create";
-import * as info from "./info";
 
-export const commands = [auth, create, info];
+export const commands = [auth, create];
