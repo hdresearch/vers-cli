1a8276758ab96b4fe09119777152e664d91d8253
Arden <ssebexen@gmail.com>
2025-05-09 17:53:46 -0400

    tiny tweak for API reasons

--- cmd/execute.go
+++ cmd/execute.go
@@ -94,11 +94,11 @@ var executeCmd = &cobra.Command{
 		apiCtx, cancel := context.WithTimeout(baseCtx, 30*time.Second)
 		defer cancel()
 
-		vm, err := client.API.Vm.Get(apiCtx, vmID)
+		response, err := client.API.Vm.Get(apiCtx, vmID)
 		if err != nil {
 			return fmt.Errorf(s.NoData.Render("failed to get VM information: %v"), err)
 		}
-		executeResult := response.Data
+		vm := response.Data
 
 		if vm.State != "Running" {
 			return fmt.Errorf(s.NoData.Render("VM is not running (current state: %s)"), vm.State)
@@ -126,13 +126,14 @@ var executeCmd = &cobra.Command{
 			}
 
 			// Get SSH key using SDK
-			sshKeyBytes, err := client.API.Vm.GetSSHKey(apiCtx, vmID)
+			response, err := client.API.Vm.GetSSHKey(apiCtx, vmID)
 			if err != nil {
 				return fmt.Errorf(s.NoData.Render("failed to get SSH key: %v"), err)
 			}
+			sshKeyBytes := response.Data
 
 			// Write key to file
-			if err := os.WriteFile(keyPath, []byte(*sshKeyBytes), 0600); err != nil {
+			if err := os.WriteFile(keyPath, []byte(sshKeyBytes), 0600); err != nil {
 				return fmt.Errorf(s.NoData.Render("failed to write key file: %v"), err)
 			}
 
