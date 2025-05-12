1844d60a087a06e5c08298526a97ec7314473a1d
Arden <ssebexen@gmail.com>
2025-05-12 14:49:44 -0400

    More old code leaking in

--- cmd/up.go
+++ cmd/up.go
@@ -1,11 +1,8 @@
 package cmd
 
 import (
-	"context"
 	"fmt"
-	"time"
 
-	vers "github.com/hdresearch/vers-sdk-go"
 	"github.com/spf13/cobra"
 )
 
@@ -16,47 +13,11 @@ var upCmd = &cobra.Command{
 	Long:  `Build a rootfs image and start a Vers development environment according to the configuration in vers.toml.`,
 	Args:  cobra.MaximumNArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
-		baseCtx := context.Background()
-
-		apiCtx, cancel := context.WithTimeout(baseCtx, 30*time.Second)
-		defer cancel()
-
-		// Create new cluster params with the mandatory KernelName field
-		clusterParams := vers.APIClusterNewParams{}
-
-		// Handle optional parameters from flags
-		memSize, _ := cmd.Flags().GetInt64("mem-size")
-		if memSize > 0 {
-			clusterParams.MemSizeMib = vers.F(memSize)
-			fmt.Printf("Setting memory size to %d MiB\n", memSize)
-		}
-
-		rootfs, _ := cmd.Flags().GetString("rootfs")
-		if rootfs != "" {
-			clusterParams.RootfsName = vers.F(rootfs)
-			fmt.Printf("Using rootfs: %s\n", rootfs)
-		}
-
-		vcpuCount, _ := cmd.Flags().GetInt64("vcpu")
-		if vcpuCount > 0 {
-			clusterParams.VcpuCount = vers.F(vcpuCount)
-			fmt.Printf("Setting vCPU count to %d\n", vcpuCount)
-		}
-
-		kernelName, _ := cmd.Flags().GetString("kernel")
-		if kernelName != "" {
-			clusterParams.KernelName = vers.F(kernelName)
-			fmt.Printf("Using kernel: %s\n", kernelName)
-		}
-
-		fmt.Println("Sending request to start cluster...")
-		clusterInfo, err := client.API.Cluster.New(apiCtx, clusterParams)
+		// Load configuration from vers.toml
+		config, err := loadConfig()
 		if err != nil {
 			return fmt.Errorf("failed to load configuration: %w", err)
 		}
-
-		// Apply flag overrides
-		applyFlagOverrides(cmd, config)
 
 		// Skip build step if rootfs is "default" or if builder is "none"
 		if config.Rootfs.Name != "default" && config.Builder.Name != "none" {
@@ -66,7 +27,7 @@ var upCmd = &cobra.Command{
 			}
 		}
 
-		// Then, run the environment
+		// Then, start the cluster
 		fmt.Println("\n=== Starting development environment ===")
 		if err := StartCluster(config, args); err != nil {
 			return fmt.Errorf("run failed: %w", err)
