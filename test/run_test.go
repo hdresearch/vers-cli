80fdbd0a83e1eecd6bca3027b484abf20b9b61f7
Justin <jgoldstein46@gmail.com>
2025-08-25 17:36:18 +0200

    start constructing pipe for standard error in run test

--- test/run_test.go
+++ test/run_test.go
@@ -2,8 +2,11 @@ package test
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"path/filepath"
+	"regexp"
+	"strings"
 	"testing"
 
 	"os/exec"
@@ -26,27 +29,15 @@ func TestStartCluster(t *testing.T) {
 
 
 	installCli(t)
-	startCluster(t)
 
-	// ./test/testdata && \
-	// vers run`,
+	login(t)
 
-	// var output, err = cmd.Output()
+// 	var got = `Sending request to start cluster...
+// Cluster (ID: cluster-abd123) started successfully with root vm 'vm-abd123'.
+//  HEAD now points to: vm-dfs34` 
 
-	// if err != nil {
-	// 	t.Fatalf("failed to start cluster. Got: %v", err)
-	// }
-
-	// var got = string(output)
-
-    // fmt.Printf("Got vers run output: %v", got)
-
-	// var want = regexp.MustCompile(string(golden.Read(t)))
-	
-	// if !want.MatchString(got) {
-	// 	t.Errorf("failed to start cluster. Want = %#q \ngot %v", want, got)
-	// }
-
+	var got = startCluster(t)
+	validateOutput(t, got)
 }
 
 func installCli(t *testing.T) {
@@ -63,13 +54,35 @@ func installCli(t *testing.T) {
 	);
 }
 
-func startCluster(t *testing.T) {
-	var goInstallPath = os.Getenv("GO_INSTALL_PATH")
-	var cliPath = fmt.Sprintf("%v/vers", goInstallPath)
-	
+func login(t *testing.T) {
+	cliPath := getCliPath()
+
+	var apiKey string = os.Getenv(("VERS_API_KEY"))
 	execCommand(
+		t, cliPath, "login", "--token", apiKey,
+	)
+}
+
+func startCluster(t *testing.T) string {
+	cliPath := getCliPath()
+	
+	return execCommand(
 		t, cliPath, "run", 
 	)
+}
+
+func getCliPath() string {
+	var goInstallPath = strings.TrimRight(os.Getenv("GO_INSTALL_PATH"), "/")
+	var cliPath = fmt.Sprintf("%v/vers", goInstallPath)
+	return cliPath
+}
+
+func validateOutput(t *testing.T, got string) {
+	var want = regexp.MustCompile(`Sending request to start cluster...\nCluster \(ID: cluster-\w+\) started successfully with root vm 'vm-\w+'\.\nHEAD now points to: vm-\w+`)
+
+	if !want.MatchString(got) {
+		t.Fatalf("Unexpected output. Want = %#q \nGot = %v", want, got)
+	}
 }
 
 func execCommand(t *testing.T, command string, args ...string) string {
@@ -79,14 +92,23 @@ func execCommand(t *testing.T, command string, args ...string) string {
 		command, args...,
 	);
 	
-	var output, err = cmd.Output()
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		t.Fatalf("Failed to create stderr pipe. Got: %v", err)
+	}
+
+	var output []byte
+
+	output, err = cmd.Output()
 
 	var stdout = string(output)
 
 	t.Logf("Got output: %v\n", stdout)
 
+	slurp, _ := io.ReadAll(stderr)
+
 	if err != nil {
-		t.Fatalf("Failed to execute command. Got error: %v\n", err)
+		t.Fatalf("Failed to execute command. Got error: %v\n", string(slurp))
 	}
 
 	return stdout
